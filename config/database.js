faffe9c87aceef6c02548f34a12b5d4fc2b57075
Diego Barrera <7diegofernando@gmail.com>
2022-08-22 21:51:33 -0500

    complete

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '@cluster0.tu4g6me.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'guitarNY_API'),
-        username: env('DATABASE_USERNAME', 'root_dbarrera'),
-        password: env('DATABASE_PASSWORD', '831601Cobq'),
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
